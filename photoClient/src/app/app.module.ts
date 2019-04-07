ab4337a6e6861aee6a8e5eecad721c3532658464
kuntal32 <kuntalstar@gmail.com>
2019-04-07 01:00:09 -0700

    add angular material

--- photoClient/src/app/app.module.ts
+++ photoClient/src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatMenuModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatTooltipModule,
+  MatDialogModule
+ } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -12,7 +26,18 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatTooltipModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
